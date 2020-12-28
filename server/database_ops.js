5955635b22193c11abcd4d67ce0701ec1d0f591e
lwh16 <lwh16@ic.ac.uk>
2020-12-28 12:30:14 +0000

    12:30 28/12/20

--- server/database_ops.js
+++ server/database_ops.js
@@ -8,7 +8,8 @@
 
 const sqlite3 = require('sqlite3')
 const path = require('path')
-const db = new sqlite3.Database('media/pi/DATABASE/Test_Database/test.db')
+const dbPath = path.resolve(__dirname, 'Test_Database/test.db')
+const db = new sqlite3.Database(dbPath)
 
 //Function to add a new reading to the db
 const insertReading = (type, reading) =>
@@ -17,21 +18,24 @@ const insertReading = (type, reading) =>
 }
 
 //Function to fetch the last n readings from the db table
-const fetchReadings = (type, limit, callback) =>
+const fetchReadings = (type, limit, 
+callback) =>
 {
 	db.all(`SELECT * FROM ${type} ORDER BY createdAt DESC LIMIT ${limit}`,
 	callback)
 }
 
 //Function to get readings between a start and end time
-const fetchReadingsBetweenTime = (type, start, end, callback) =>
+const fetchReadingsBetweenTime = (type, start, end, 
+callback) =>
 {
-	db.all(`SELECT * FROM data WHERE createdAt > ? AND createdAT < ?`;,
+	db.all(`SELECT * FROM data WHERE createdAt > ? AND createdAT < ?;`,
 	[start, end],callback)
 }
 
 //Function to get the average of all the readings between a set of times
-const getAvgReadingBetweenTime = (type, start, end, callback) =>
+const getAvgReadingBetweenTime = (type, start, end, 
+callback) =>
 {
 	db.get(`SELECT avg(value) FROM data WHERE createdAt > ? AND createdAt < ?;`,
 	[start, end], callback)
