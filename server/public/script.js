f0be7892c5f38ffec4ad0e4b04a986b84218df22
lwh16 <60610959+lwh16@users.noreply.github.com>
2020-12-28 16:13:45 +0000

    Update script.js

--- server/public/script.js
+++ server/public/script.js
@@ -55,6 +55,81 @@ const fetchDataHistory = () =>
 	})
 }
 
+/**
+* First, define a function that will initialize the 
+socket connection and add listeners
+* to the required events
+*/
+const addSocketListeners = () => {
+/**
+* The "io" constructor is available to us after 
+including the socket.io library script in the     "index.html" file
+* Initializing the socket connection is as easy as the 
+statement below
+*/
+const socket = io()
+
+/**
+* An event listener is attached to the "new-
+temperature" event
+* The handler is similar to the handler that was attached 
+to the GET /temperature API, so in essence, we are 
+replacing the API call with the socket event notification
+*/
+socket.on('new-temperature', data => {
+const now = new Date()
+const timeNow =
+now.getHours() + ':' + now.getMinutes() + ':' +     now.getSeconds()
+pushData(temperatureChartConfig.data.labels, timeNow, 
+10)
+pushData(temperatureChartConfig.data.datasets[0].data, 
+data.value, 10)
+
+temperatureChart.update()
+temperatureDisplay.innerHTML = '<strong>' +
+data.value + '</strong>'
+})
+
+/**
+* Similarly, we add the handler for the "new-humidity" 
+event
+*/
+socket.on('new-humidity', data => {
+const now = new Date()
+const timeNow =
+now.getHours() + ':' + now.getMinutes() + ':' +
+now.getSeconds()
+pushData(humidityChartConfig.data.labels, timeNow, 
+10)
+pushData(humidityChartConfig.data.datasets[0].data, 
+data.value, 10)
+
+humidityChart.update()
+humidityDisplay.innerHTML = '<strong>' + data.value + 
+'</strong>'
+})
+}
+
+if (!getParameterByName('start') &&     !getParameterByName('end')) {
+/**
+* Finally, the fetchHumidity and fetchTemperature 
+functions, that used to call the APIs at regular     intervals, are removed.
+* In their place, the addSocketListeners function is
+called (and only needs to be called once this time)
+*/
+addSocketListeners()
+
+// setInterval(() => {
+// fetchTemperature()
+// fetchHumidity()
+// }, 2000)
+fetchHumidityHistory()
+fetchTemperatureHistory()
+} else {
+fetchHumidityRange()
+fetchTemperatureRange()
+}
+
 function getParameterByName (name)
 {
 	//pulled from tutorial
